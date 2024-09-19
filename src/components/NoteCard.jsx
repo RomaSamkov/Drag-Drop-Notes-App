9f972be37d2f8643032c899db535ba107dee842c
RomaSamkov <Romansamkov.rs@gmail.com>
2024-09-19 20:59:58 +0300

    add autoGrow, useRef and useEffect

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from "react";
 import Trash from "../icons/Trash";
 
 /* eslint-disable react/prop-types */
@@ -5,6 +6,18 @@ const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
   const position = JSON.parse(note.position);
   const colors = JSON.parse(note.colors);
+
+  const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    autoGrow(textAreaRef);
+  }, []);
+
+  const autoGrow = (textAreaRef) => {
+    const { current } = textAreaRef;
+    current.style.height = "auto"; // Reset the height
+    current.style.height = current.scrollHeight + "px"; // Set the new height
+  };
 
   return (
     <div
@@ -23,8 +36,12 @@ const NoteCard = ({ note }) => {
       </div>
       <div className="card-body">
         <textarea
+          ref={textAreaRef}
           style={{ color: colors.colorText }}
           defaultValue={body}
+          onInput={() => {
+            autoGrow(textAreaRef);
+          }}
         ></textarea>
       </div>
     </div>
