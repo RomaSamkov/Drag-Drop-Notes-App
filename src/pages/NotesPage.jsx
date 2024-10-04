48f2da3f55b1574dd67681a62ee556a9932efafc
RomaSamkov <Romansamkov.rs@gmail.com>
2024-10-04 17:11:41 +0300

    add init

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,7 +1,23 @@
-import { fakeData as notes } from "../assets/fakeData";
+// import { fakeData as notes } from "../assets/fakeData";
+import { databases } from "../appwrite/config";
+import { useEffect, useState } from "react";
 import NoteCard from "../components/NoteCard";
 
 const NotesPage = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    const response = await databases.listDocuments(
+      import.meta.env.VITE_DATABASE_ID,
+      import.meta.env.VITE_COLLECTION_NOTES_ID
+    );
+
+    setNotes(response.documents);
+  };
   return (
     <div id="app">
       {notes.map((note) => (
