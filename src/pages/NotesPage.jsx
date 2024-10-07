e7683648c1f62caab426c7571ef3cd29efb0c1c1
RomaSamkov <Romansamkov.rs@gmail.com>
2024-10-07 15:48:48 +0300

    Update NotesPage.jsx

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -2,6 +2,7 @@
 import { databases } from "../appwrite/config";
 import { useEffect, useState } from "react";
 import NoteCard from "../components/NoteCard";
+import { db } from "../appwrite/databases";
 
 const NotesPage = () => {
   const [notes, setNotes] = useState([]);
@@ -11,10 +12,11 @@ const NotesPage = () => {
   }, []);
 
   const init = async () => {
-    const response = await databases.listDocuments(
-      import.meta.env.VITE_DATABASE_ID,
-      import.meta.env.VITE_COLLECTION_NOTES_ID
-    );
+    const response = await db.notes.list();
+    // const response = await databases.listDocuments(
+    //   import.meta.env.VITE_DATABASE_ID,
+    //   import.meta.env.VITE_COLLECTION_NOTES_ID
+    // );
 
     setNotes(response.documents);
   };
