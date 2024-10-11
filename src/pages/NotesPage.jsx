834f3a741a085594b3606ef844751f694b8c9495
RomaSamkov <Romansamkov.rs@gmail.com>
2024-10-11 20:15:01 +0300

    add NoteContext

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,29 +1,13 @@
-// import { fakeData as notes } from "../assets/fakeData";
-// import { databases } from "../appwrite/config";
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import NoteCard from "../components/NoteCard";
-import { db } from "../appwrite/databases";
+import { NoteContext } from "../context/NoteContext";
 
 const NotesPage = () => {
-  const [notes, setNotes] = useState([]);
-
-  useEffect(() => {
-    init();
-  }, []);
-
-  const init = async () => {
-    const response = await db.notes.list();
-    // const response = await databases.listDocuments(
-    //   import.meta.env.VITE_DATABASE_ID,
-    //   import.meta.env.VITE_COLLECTION_NOTES_ID
-    // );
-
-    setNotes(response.documents);
-  };
+  const { notes } = useContext(NoteContext);
   return (
     <div id="app">
       {notes.map((note) => (
-        <NoteCard key={note.$id} note={note} setNotes={setNotes} />
+        <NoteCard key={note.$id} note={note} />
       ))}
     </div>
   );
