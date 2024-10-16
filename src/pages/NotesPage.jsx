be3eb296156800a2c71ec1dc7b40d868841fdcbd
RomaSamkov <Romansamkov.rs@gmail.com>
2024-10-16 21:04:52 +0300

    add Controls

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import NoteCard from "../components/NoteCard";
 import { NoteContext } from "../context/NoteContext";
+import Controls from "../components/Controls";
 
 const NotesPage = () => {
   const { notes } = useContext(NoteContext);
@@ -9,6 +10,7 @@ const NotesPage = () => {
       {notes.map((note) => (
         <NoteCard key={note.$id} note={note} />
       ))}
+      <Controls />
     </div>
   );
 };
