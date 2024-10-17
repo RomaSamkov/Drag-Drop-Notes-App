314e024c30b100c86bbeb4404842732625a78410
RomaSamkov <Romansamkov.rs@gmail.com>
2024-10-17 20:38:52 +0300

    Update NoteContext.jsx

--- src/context/NoteContext.jsx
+++ src/context/NoteContext.jsx
@@ -7,6 +7,7 @@ export const NoteContext = createContext();
 const NoteProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [notes, setNotes] = useState([]);
+  const [selectedNote, setSelectedNote] = useState(null);
 
   useEffect(() => {
     init();
@@ -22,7 +23,7 @@ const NoteProvider = ({ children }) => {
     setLoading(false);
   };
 
-  const contextData = { notes, setNotes };
+  const contextData = { notes, setNotes, selectedNote, setSelectedNote };
   return (
     <NoteContext.Provider value={contextData}>
       {loading ? (
